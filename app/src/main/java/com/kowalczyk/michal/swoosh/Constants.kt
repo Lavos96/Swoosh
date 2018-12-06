f76dbee52bcb70fa515f9cc4ec9f98ba2200ded7
Michał Kowalczyk <lavos1996@o2.pl>
2018-12-06 11:46:10 +0100

    Added button logic

---
+++ app/src/main/java/com/kowalczyk/michal/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.kowalczyk.michal.swoosh
+
+const val EXTRA_LEAGUE="league"
--- app/src/main/java/com/kowalczyk/michal/swoosh/SkillActivity.kt
+++ app/src/main/java/com/kowalczyk/michal/swoosh/SkillActivity.kt
@@ -3,10 +3,16 @@ package com.kowalczyk.michal.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
+
+    //rozpakowywanie zmiennych przeslanych z leagueActivity
+
+    var league=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league=intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
     }
 }
